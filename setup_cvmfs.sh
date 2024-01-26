4f236d52992e878a50cc4788dca4f6ea01d826cc
Patrick Rieck <patrick.rieck@cern.ch>
2024-01-25 20:39:57 -0600

    HepMC3 from cvmfs

--- setup_cvmfs.sh
+++ setup_cvmfs.sh
@@ -3,11 +3,11 @@ source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7
 cd /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/share/Geant4-10.5.1/geant4make/
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/share/Geant4-10.5.1/geant4make/geant4make.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/ROOT/6.18.04/x86_64-centos7-gcc8-opt/ROOT-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/HepMC/2.06.10/x86_64-centos7-gcc8-opt/HepMC-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/jsoncpp/1.8.4/x86_64-centos7-gcc8-opt/jsoncpp-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/CMake/3.14.3/x86_64-centos7-gcc8-opt/CMake-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/cmaketools/1.8/x86_64-centos7-gcc8-opt/cmaketools-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/301/x86_64-centos7-gcc8-opt/pythia8env-genser.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/fastjet/3.3.2/x86_64-centos7-gcc8-opt/fastjet-env.sh
+export HEPMC_HOME=/cvmfs/sft.cern.ch/lcg/views/LCG_97/x86_64-centos7-gcc8-opt/
 export PYTHIA8DATA=$PYTHIA8_HOME/share/Pythia8/xmldoc   
 cd $CURRENTDIR
